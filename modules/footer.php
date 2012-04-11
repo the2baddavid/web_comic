60f8f2d97816c1dfa2a010764a0f925b0b070080
david bartz <the2baddavid@gmail.com>
2012-04-11 17:09:33 -0400

    Added About, Comics, & Extras Pages, Updated Menus

--- modules/footer.php
+++ modules/footer.php
@@ -9,10 +9,10 @@ function display_footer()
 {
 echo <<< _END
 		<div id="footer" class="span-24">
-                    <a>Home</a>
-                    <a>Comics</a>
-                    <a>Extras</a>
-                    <a>About</a>
+                    <a href="index.php" id="alt-menu-home">Home</a>
+                    <a href="comics.php" id="alt-menu-comics">Comics</a>
+                    <a href="extras.php" id="alt-menu-extras">Extras</a>
+                    <a href="about.php" id="alt-menu-about">About</a>
                     <table>
                         <td>Copyright: Apex Copics</td>
                         <td>Design by: Ascii Abstractions</td>
--- modules/menu.php
+++ modules/menu.php
@@ -10,10 +10,10 @@ function display_menu()
     echo <<< _END
     <div id="menu" class="spawn-24">
         <table>
-            <td><a href="/" id="menu-home">Home</a></td>
-            <td><a href="/archive" id="menu-comics">Comics</a></td>
-            <td><a href="/extras" id="menu-extras">Extras</a></td>
-            <td><a href="/about" id="menu-about">About</a></td>
+            <td><a href="index.php" id="menu-home">Home</a></td>
+            <td><a href="comics.php" id="menu-comics">Comics</a></td>
+            <td><a href="extras.php" id="menu-extras">Extras</a></td>
+            <td><a href="about.php" id="menu-about">About</a></td>
         </table>
     </div>
 _END;
