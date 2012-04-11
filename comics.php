d423dd4f6c38b167b885477bae932f1f0dab16d8
david bartz <the2baddavid@gmail.com>
2012-04-11 17:12:10 -0400

    Added Framework for new pages: about, comics,extras

--- comics.php
+++ comics.php
@@ -4,4 +4,26 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+include_once('modules/modules.php');
+include_once('database/db_object.php');  
+
+$server_name='localhost';
+$db_name='';
+$user='user';
+$pass='pass';
+
+$db = new DB_OBJECT($server_name, $db_name, $user, $pass);
+
+display_start();    // No Span
+display_header();   // Span should be 24, the entire page
+display_menu();     // if horizontal then this should also span 24
+?>
+<!-- Stuff that is specific to a page goes here -->
+
+<!-- Back to Php for the footer! -->
+<?php
+
+display_footer();
+
 ?>
